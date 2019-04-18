2d4aded7c9b68b8d798dee4e84ac704967d2f41c
songyichao <songyichao74@gmail.com>
2019-04-18 14:19:19 +0800

    learn

--- AsciiJSON.go
+++ AsciiJSON.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "go",
+			"tag":  "<br>",
+		}
+		c.AsciiJSON(http.StatusOK, data)
+	})
+	r.Run(":8080")
+}
--- html.go
+++ html.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	//单文件
+	//router.LoadHTMLGlob("templates/*")
+	//router.GET("/index", func(c *gin.Context) {
+	//	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+	//		"title": "main website",
+	//	})
+	//})
+	//多文件
+	router.LoadHTMLGlob("templates/**/*")
+	router.GET("/posts/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{
+			"title": "Posts",
+		})
+	})
+	router.GET("/users/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
+			"title": "Users",
+		})
+	})
+	router.Run(":8080")
+}
